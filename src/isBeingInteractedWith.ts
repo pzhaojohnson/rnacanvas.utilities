218e07838bcd83eb6dbf574cd98189c227be438e
Philip Johnson <p.zhao.johnson@gmail.com>
2024-09-29 14:17:52 -0400

    Add comment

--- src/isBeingInteractedWith.ts
+++ src/isBeingInteractedWith.ts
@@ -5,6 +5,9 @@ window.addEventListener('mousedown', event => prevMouseDown = event);
 /**
  * Returns true if and only if the provided element (or any of its child elements)
  * are currently being interacted with by the user.
+ *
+ * Note that this function may not work properly with lazy loading or dynamic imports
+ * (since it only begins tracking which element is being interacted with after being imported).
  */
 export function isBeingInteractedWith(ele: Element): boolean {
   if (ele === document.body) { return true; }
