a0bbf0d2b2c79f221abde6a4bdf72ac823ba7019
Philip Johnson <p.zhao.johnson@gmail.com>
2024-10-29 20:33:37 -0400

    Fix handling of when nodes are removed from the DOM tree in isBeingInteractedWith() function

--- src/isBeingInteractedWith.ts
+++ src/isBeingInteractedWith.ts
@@ -1,20 +1,35 @@
-let prevMouseDown: MouseEvent | null = null;
+// must cache the nodes being interacted with at the time of mouse down
+// (in case nodes being interacted with were to be removed from the DOM tree)
+let nodesBeingInteractedWith: Set<Node> = new Set();
 
-window.addEventListener('mousedown', event => prevMouseDown = event);
+window.addEventListener('mousedown', event => {
+  if (!(event.target instanceof Node)) {
+    nodesBeingInteractedWith = new Set();
+    return;
+  }
+
+  nodesBeingInteractedWith = new Set();
+
+  let node = event.target;
+  nodesBeingInteractedWith.add(node);
+
+  // check for circular node hierarchies (just in case)
+  while (node.parentNode && !nodesBeingInteractedWith.has(node.parentNode)) {
+    nodesBeingInteractedWith.add(node.parentNode);
+    node = node.parentNode;
+  }
+});
 
 /**
  * Returns true if and only if the provided element (or any of its child elements)
  * are currently being interacted with by the user.
  *
  * Note that this function may not work properly with lazy loading or dynamic imports
- * (since it only begins tracking which element is being interacted with after being imported).
+ * (since it only begins tracking which elements are being interacted with after being imported).
  */
 export function isBeingInteractedWith(ele: Element): boolean {
-  if (ele === document.body) { return true; }
-
   return (
-    !!prevMouseDown
-    && prevMouseDown.target instanceof Node
-    && ele.contains(prevMouseDown.target)
-  );
+    ele === document.body
+    || nodesBeingInteractedWith.has(ele)
+  )
 }
